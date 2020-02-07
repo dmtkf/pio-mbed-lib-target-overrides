5eba976dd2c652776234246fa46f51563d8df582
Dmitrii Tokarev <dtokarev@seglobal.com.sg>
2020-02-07 11:06:28 +0800

    Separate overrides by boards instead of environments

--- tools/mbed_config_override.h
+++ tools/mbed_config_override.h
@@ -1,2 +1,2 @@
-// common overrides for all environments
+// common overrides for all boards
 #define MBED_CONF_CMNLIB_PARAM  200   // comment
--- tools/post_script_mbed_config.py
+++ tools/post_script_mbed_config.py
@@ -1,90 +1,74 @@
 from os.path import basename, join
 import inspect
-from fileinput import FileInput
-import re
 
 Import('env')
 
-# dictionary for overrides
-mbed_config_overrides =	{
-}
-
 #
-# extract defines from header file
+# add undef for each define in header file
 #
 def parse_mbed_config_override_h(fname):
-    defines = {}
+    content = []
     try:
         with open(fname) as f:
-            for line in f:
+            lines = f.readlines()
+            for line in lines:
+                # looking for lines with #define
                 if line.startswith('#define'):
-                    # split string into columns
-                    columns = []
-                    # column may have characters from a to Z, digits from 0-9, the underscore _ character
-                    # or # or / or : or |
-                    for x in re.finditer('(\w|#|/|:|\|)+', line):
-                        columns.append(x.group())
-                    # add param:value into defines dictionary
-                    if (len(columns) >= 3):
-                        defines.update({columns[1]: columns[2]})
+                    # add #undef for found define
+                    content.append('#undef ' + line.split()[1] + '\n')
+            # add original file content after all #undef
+            content.extend(lines)
+            content.append('\n')
     except:
         pass
     
-    return defines
+    return content
+
 
 #
-# process mbed_config.h and replace all defines found in overrides
+# process mbed_config.h and add all defines found in overrides
 #
 def process(overrides):
     # full path to mbed_config.h
     mbed_config = join(env.subst('$BUILD_DIR'), 'mbed_config.h')
 
-    # just some default values
-    width_param = width_val = 1
+    content = []
+    try:
+        with open(mbed_config) as f:
+            content = f.readlines()
+    except:
+        pass
 
-    # we will see the name of pio environment and this script name as the cause of overridding
-    env_script_name = env['PIOENV'] + ':' + basename(inspect.getfile(inspect.currentframe()))
+    if len(content):
+        # go up from end to look for #endif
+        i = len(content) - 1
+        while (i and not content[i].startswith('#endif')):
+            i -= 1
 
-    # loop throug each line of file
-    for line in FileInput(mbed_config, inplace=True):
-        line = line.rstrip('\r\n')
-        newline = None
-        for key in overrides.keys():
-            if line.startswith('#define'):
-                # if widths still have default values
-                if width_param == 1 or width_val == 1:
-                    # split string into columns
-                    columns = []
-                    # column may have characters from a to Z, digits from 0-9, the underscore _ character
-                    # or # or / or : or |
-                    for x in re.finditer('(\w|#|/|:|\|)+', line):
-                        columns.append(x.start())
-                    if (len(columns) >= 4):
-                        # decrease by 1 as format string has one whitespace
-                        # in case if widths will not be set
-                        width_param = columns[2] - columns[1] - 1
-                        width_val = columns[3] - columns[2] - 1
+        # stick to #endif line
+        i -= len(content)
+        # insert all overrides
+        content[i:i] = '// platformio.ini:' + basename(inspect.getfile(inspect.currentframe())) + '\n'
+        content[i:i] = overrides
 
-            # parameter redefinition
-            if key in line:
-                newline = '#define {:{wp}} {:{wv}} // set by {}'.format(key, str(mbed_config_overrides[key]),
-                            env_script_name, wp=width_param, wv=width_val)
+        with open(mbed_config, "w") as f:
+            f.writelines(content)
 
-        # write processed or original line back into file
-        print(newline or line)
+# list for overrides
+mbed_config_overrides =	[]
 
 # path for header files
 base_path = join(env['PROJECT_DIR'], 'tools')
 # base filename for header files
 base_name = 'mbed_config_override'
 
-# extract defines from file for all environments
+# extract defines from file for all boards
 common_h = join(base_path, base_name + '.h')
-mbed_config_overrides.update(parse_mbed_config_override_h(common_h))
+mbed_config_overrides.extend(parse_mbed_config_override_h(common_h))
 
-# extract defines from file for current environment
-environment_h = join(base_path, base_name + '_' + env['PIOENV'] + '.h')
-mbed_config_overrides.update(parse_mbed_config_override_h(environment_h))
+# extract defines from file for project's board
+board_h = join(base_path, base_name + '_' + env.BoardConfig().id + '.h')
+mbed_config_overrides.extend(parse_mbed_config_override_h(board_h))
 
 # process overrides if we have defines for that
 if (len(mbed_config_overrides)):
